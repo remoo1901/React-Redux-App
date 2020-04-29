b95b73a16d48c3856f5d5335a3de1814eadca7bb
remoo1901 <remoo1901@hotmail.com>
2020-04-28 20:45:19 -0400

    finished actions

---
+++ react-redux/src/actions/index.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export const setEditTrue = () => {
+  return { type: "EDITING" };
+};
+
+export const setEditFalse = () => {
+  return (dispatch) => {
+    dispatch({ type: "LOADING" });
+    setTimeout(() => {
+      dispatch({ type: "DONE_EDITING" });
+    }, 5000);
+  };
+};
+
+export const getDog = () => {
+  return (dispatch) => {
+    dispatch({ type: "LOADING" });
+    axios
+      .get("https://dog.ceo/api/breeds/image/random")
+      .then((res) => dispatch({ type: "NEW_DOG", payload: res.data.message }))
+      .catch((err) => console.log(ERROR, err));
+  };
+};
