373ba53505b505214875cb6254c99871598c94f0
remoo1901 <remoo1901@hotmail.com>
2020-04-28 20:21:09 -0400

    working on reducers

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-
+import DogPic from "./components/DogPic"
+import Title from "./components/Title"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>APP</h1>
+      <div>  
+      <Title />
+      <DogPic />
+
+      </div>
       </header>
     </div>
   );
