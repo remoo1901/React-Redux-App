8ac12a96a76aa3e42a35a1ca12238b893904d04f
remoo1901 <remoo1901@hotmail.com>
2020-04-28 20:33:20 -0400

    finished reducers

---
+++ react-redux/src/reducers/index.js
@@ -0,0 +1,25 @@
+const initialState = {
+  title: "here's the title",
+  editing: false,
+  loading: false,
+  dog: "https://images.dog.ceo/breeds/poodle-toy/n02113624_681.jpg",
+};
+
+export default titleReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "EDITING":
+      return { ...state, editing: true };
+
+    case "DONE_EDITING":
+      return { ...state, editing: false };
+
+    case "LOADING":
+      return { ...state, loading: true };
+
+    case "NEW_DOG":
+      return { ...state, dog: action.payload };
+
+    default:
+      return state;
+  }
+};
